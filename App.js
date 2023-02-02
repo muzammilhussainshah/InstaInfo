5c3499bc3b5049f72acbb52dac73c545b5bda9ce
muzammil <muzammil@muzammils-MacBook-Pro.local>
2023-02-02 07:05:07 -0800

    code [FORMAT]

--- App.js
+++ App.js
@@ -8,31 +8,21 @@ import {
 } from 'react-native-safe-area-context';
 import Navigation from './src/router/Tab';
 import { PortalProvider } from '@gorhom/portal';
-// import store from './src/store';
-
-// import { Provider } from 'react-redux';
 import { LogBox, StatusBar, View } from 'react-native';
 const cheerio = require('react-native-cheerio')
 
 function App() {
   useEffect(async () => {
-
-
     LogBox.ignoreLogs(['Remote debugger']);
   }, [])
   
 
   return (
-    // <Provider store={store}>
-      // <StatusBar
-      //   hidden={true}
-      // />
       <SafeAreaProvider>
         <PortalProvider>
           <Navigation />
         </PortalProvider>
       </SafeAreaProvider>
-    // </Provider>
   );
 }
 
