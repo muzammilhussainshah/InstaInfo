2ffa835b3e88fbba584840d021dd11720606530f
muzammil <muzammil@muzammils-MacBook-Pro.local>
2023-02-01 16:23:50 -0800

    searchScreen[ADD]

--- src/Screen/Search/Search.js
+++ src/Screen/Search/Search.js
@@ -7,17 +7,33 @@ import {
   FlatList,
   View,
   AsyncStorage,
-  Text
+  Text,
+  TouchableOpacity
 } from 'react-native';
+
+// import { Button } from 'react-native-paper';
 import Colors from '../../styles/Colors';
 import { styles } from './styles';
 
-
 const Search = ({ navigation }) => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const onChangeSearch = query => setSearchQuery(query);
+
   return (
     <View style={styles.container}>
-      <Text>{`Search`}</Text>
-      <View style={{ height: 50, backgroundColor: Colors.tabInactive, width: '70%', borderRadius: 15 }}></View>
+
+      <View style={{ height: 50, backgroundColor: Colors.black, width: '70%', borderRadius: 15 }}></View>
+      <TouchableOpacity
+        activeOpacity={0.9}
+        style={{ backgroundColor: Colors.black, color: Colors.red, paddingHorizontal: 25,marginTop:10, paddingVertical: 10, borderRadius: 10 }}>
+        <Text style={{ color: Colors.tabInactive, fontSize: 20 }}>{`Search`}</Text>
+      </TouchableOpacity>
+      {/* <Button style={{ backgroundColor: Colors.tabInactive, color: Colors.red }}
+      title='Search'
+      color={Colors.red}
+      backgroundColor="red"
+    /> */}
     </View >
   );
 };
--- src/Screen/Search/styles.js
+++ src/Screen/Search/styles.js
@@ -3,7 +3,7 @@ import { RFPercentage } from 'react-native-responsive-fontsize';
 import Colors from '../../styles/Colors';
 export const styles = StyleSheet.create({
     container: {
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.tabBg,
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
